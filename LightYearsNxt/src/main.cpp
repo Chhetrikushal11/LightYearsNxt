7c57b4ff3319153c9bf1e64a73ea652448a2adf1
KUSHAL <chhetrikushal11@gmail.com>
2025-10-07 18:54:36 -0400

    Build the automatic postBuild macro to link dll libraries

--- LightYearsNxt/src/main.cpp
+++ LightYearsNxt/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow renderWindow(sf::VideoMode(1040, 1280), "MyWindow");
 	std::cout << "Hello Everyone in Light Years Nxt Game." << std::endl;
 	return 0;
 }
