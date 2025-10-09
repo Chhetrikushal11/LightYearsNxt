09804d3b75f3a7e5063c9ec63cf6b2d83ed464f3
KUSHAL <chhetrikushal11@gmail.com>
2025-10-09 16:27:30 -0400

    Create unique_ptr instead of shared_ptr.

--- LightYearsNxt/src/main.cpp
+++ LightYearsNxt/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <SFML/Graphics.hpp>
 #include "framework/Application.h"
 
@@ -6,7 +7,7 @@ int main()
 {
 	// to eradicate any memory leak lets use the shared pointer
 	// as it deletes it self after it goes out of scope
-	std::shared_ptr<ly::Application> app = std::make_shared<ly::Application>(); // app is name of the Application object
+	std::unique_ptr<ly::Application> app = std::make_unique<ly::Application>(); // app is name of the Application object
 
 	app->Run();
 	return 0;
