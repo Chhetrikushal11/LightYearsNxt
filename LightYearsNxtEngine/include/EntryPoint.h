89a6ae28e90237ad5189c0df60e0a0fd21e67cf0
KUSHAL <chhetrikushal11@gmail.com>
2025-10-10 16:33:14 -0400

    SEPERATE THE ENGINE AND THE GAME.

---
+++ LightYearsNxtEngine/include/EntryPoint.h
@@ -0,0 +1,15 @@
+#pargma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "framework/Application.h"
+
+
+namespace ly
+{
+	class Application;
+
+}
+
+extern ly::Application* GetApplication();
---
+++ LightYearsNxtEngine/src/EntryPoint.cpp
@@ -0,0 +1,10 @@
+#include "EntryPoint.h"
+int main() 
+{
+	ly::Application* app = GetApplication()
+	{
+		app->Run();
+		delete app;
+	}
+}
+
